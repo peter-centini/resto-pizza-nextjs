889e6a30a4c11cfa73e3c8aaa8ed7c4b4c9c5755
Peter <peter@MacBook-Air-de-Peter.local>
2022-02-03 17:51:13 +0100

    navbar-ok

---
+++ components/Featured.jsx
@@ -0,0 +1,25 @@
+import styles from "../styles/Featured.module.css"
+import Image from "next/image";
+
+const Featured = () => {
+    const images =[
+        "/img/featured.png",
+        "/img/featured2.png",
+        "/img/featured2.png",
+    ];
+  return (
+  <div className={styles.container}>
+     <Image src="/img/arrowl.png" alt="" layout='fill'/>
+     <div className={styles.wrapper}>
+         <div className={styles.imgContainer}>
+             {images.map((img, index) => (
+            <Image src="/img/featured.png" key={index} alt="" layout='fill' />
+             ))}
+         </div>
+        </div>
+     <Image src="/img/arrowr.png" alt="" layout='fill' />
+  </div>
+  )
+};
+
+export default Featured;
---
+++ components/Layout.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Footer from './Footer';
+import Navbar from './Navbar';
+
+const Layout = ({ children }) => {
+    return (
+        <div>
+            <>
+                <Navbar />
+                { children }
+                <Footer />
+            </>
+        </div>
+    );
+};
+
+export default Layout;
