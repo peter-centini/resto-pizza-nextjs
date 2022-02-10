db3c181ededbb900f50210fc7d30c64b384b5e2d
Peter <peter@MacBook-Air-de-Peter.local>
2022-02-10 20:51:27 +0100

    all css finished

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -8,17 +8,17 @@ const Navbar = () => {
           <Image src="/img/telephone.png" alt="" width="32" height="32" />
         </div>
         <div className={styles.texts}>
-          <div className={styles.text}> Order Now! </div>
+          <div className={styles.text}> Passez commande </div>
           <div className={styles.text}> 01.02.03.04.05 </div>
         </div>
       </div>
       <div className={styles.item}>
         <ul className={styles.list}>
-          <li className={styles.listItem}>Homepage</li>
-          <li className={styles.listItem}>Products</li>
-          <li className={styles.listItem}>Menu</li>
-          <Image src="/img/logo.png" alt="" width="160px" height="69px"/>
-          <li className={styles.listItem}>Events</li>
+          <li className={styles.listItem}> Home</li>
+          <li className={styles.listItem}> Produits</li>
+          <li className={styles.listItem}> Menu</li>
+          <Image src="/img/logo2.png" alt="" width="160px" height="69px"/>
+          <li className={styles.listItem}>Evénéments</li>
           <li className={styles.listItem}>Blog</li>
           <li className={styles.listItem}>Contact</li>
         </ul>
