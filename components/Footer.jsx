5bc34469c99d5958c4bafbe96e40a2481b25b1ce
Peter <peter@MacBook-Air-de-Peter.local>
2022-02-10 14:32:33 +0100

    product finished mise en forme doc

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -10,11 +10,11 @@ const Footer = () => {
       <div className={styles.item}>
         <div className={styles.card}>
           <h2 className={styles.motto}>
-            OH YES, WE DID.THE LAMA PIZZA, WELL BAKED SLICE OF PIZZA.
+            OH OUI, NOUS L'AVONS FAIT. LA PIZZA LAMA, TRANCHE DE PIZZA BIEN CUIT.
           </h2>
         </div>
         <div className={styles.card}>
-          <h1 className={styles.title}>FIND OUR RESTAURANTS</h1>
+          <h1 className={styles.title}>TROUVER NOS RESTAURANTS</h1>
           <p className={styles.text}>
             1654 R. Don Road #304.
             <br /> NewYork, 85022
@@ -37,13 +37,13 @@ const Footer = () => {
           </p>
         </div>
         <div className={styles.card}>
-          <h1 className={styles.title}>WORKING HOURS</h1>
+          <h1 className={styles.title}>HORAIRES D'OUVERTURE </h1>
           <p className={styles.text}>
-            MONDAY UNTIL FRIDAY
+            DU LUNDI AU VENDREDI DE
             <br /> 9:00 – 22:00
           </p>
           <p className={styles.text}>
-            SATURDAY - SUNDAY
+             SAMEDI - DIMANCHE
             <br /> 12:00 – 24:00
           </p>
         </div>
---
+++ pages/Cart.jsx
@@ -0,0 +1,16 @@
+import styles from "../styles/Cart.module.css";
+import Image from "next/image";
+
+
+
+
+const Cart = () => {
+  return (
+  <div className={styles.container}>
+
+  </div>
+
+  )
+};
+
+export default Cart;
